a26f7a4de51d94032b931bbddcf6ba037da0c28f
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-01-05 12:28:30 -0500

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Locomotive::Application.config.secret_key_base = 'f98adb25fb82a78ed7cbde50e29ef514c7edd0d0f1ab71194624ac626d81bfe35b9afe0a2c2696c63d931d62b4ad8f94f1fbd507d37b43afda75787542d46aeb'
